873a4b563bf7472b9e9ae535e8c2dbbfaf3c8e3a
Sam Poder <sam.r.poder@gmail.com>
2026-07-20 17:33:49 -0700

    [turbopack] Track reexports in `import_usage`

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/reexport-drop/basic/output/1do3_crates_turbopack-tests_tests_snapshot_reexport-drop_basic_input_lib_02nzs5r.js
@@ -0,0 +1,25 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push(["output/1do3_crates_turbopack-tests_tests_snapshot_reexport-drop_basic_input_lib_02nzs5r.js",
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/reexport-drop/basic/input/lib.js [test] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/reexport-drop/basic/input/lib.js [test] (ecmascript)
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/reexport-drop/basic/input/lib.js [test] (ecmascript) <locals>
+;
+const used = 'USED';
+;
+__turbopack_context__.s([
+    "used",
+    0,
+    used
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/reexport-drop/basic/input/lib.js [test] (ecmascript) <locals>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$reexport$2d$drop$2f$basic$2f$input$2f$lib$2e$js__$5b$test$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/reexport-drop/basic/input/lib.js [test] (ecmascript) <locals>");
+__turbopack_context__.s([
+    "used",
+    0,
+    used
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/reexport-drop/basic/input/lib.js [test] (ecmascript)");
+}),
+]);
+
+//# sourceMappingURL=1do3_crates_turbopack-tests_tests_snapshot_reexport-drop_basic_input_lib_02nzs5r.js.map
