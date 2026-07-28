3d122c39dde12be8281772d354950502a5294f72
Tim Neutkens <tim@timneutkens.nl>
2026-07-28 14:40:58 -0400

    Support configurable decorator versions

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -1806,6 +1806,8 @@ export interface NextConfig {
     styledComponents?: boolean | StyledComponentsConfig
     emotion?: boolean | EmotionConfig
 
+    decoratorVersion?: 'legacy' | '2021-12' | '2022-03'
+
     styledJsx?:
       | boolean
       | {
