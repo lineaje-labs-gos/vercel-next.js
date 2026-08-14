bfd8a522f7b36370efff51984751a7e66b4ce051
Tim Neutkens <tim@timneutkens.nl>
2026-08-14 14:28:21 +0200

    Version adapter routing for Pages API routes

--- packages/next/src/build/adapter/build-complete.ts
+++ packages/next/src/build/adapter/build-complete.ts
@@ -9,6 +9,7 @@ import { interopDefault } from '../../lib/interop-default'
 import type { RouteHas } from '../../lib/load-custom-routes'
 import { recursiveReadDir } from '../../lib/recursive-readdir'
 import { isDynamicRoute } from '../../shared/lib/router/utils'
+import { isAPIRoute } from '../../lib/is-api-route'
 import type { Revalidate } from '../../server/lib/cache-control'
 import type { NextConfigComplete } from '../../server/config-shared'
 import {
@@ -518,6 +519,11 @@ export interface NextAdapter {
   ) => Promise<NextConfigComplete> | NextConfigComplete
   onBuildComplete?: (ctx: {
     routing: {
+      /**
+       * Version of the routing manifest contract. Unversioned manifests use
+       * the legacy version 1 semantics.
+       */
+      version: 2
       beforeMiddleware: Array<Route>
       /**
        * middlewareMatchers are the middleware matcher definitions emitted by
@@ -2101,7 +2107,7 @@ export async function handleBuildComplete({
     ]
 
     for (const route of routesManifest.dynamicRoutes) {
-      const shouldLocalize = Boolean(config.i18n)
+      const shouldLocalize = Boolean(config.i18n && !isAPIRoute(route.page))
 
       const routeRegex = getNamedRouteRegex(route.page, {
         prefixRouteKeys: true,
@@ -2301,6 +2307,7 @@ export async function handleBuildComplete({
 
       await adapterMod.onBuildComplete({
         routing: {
+          version: 2,
           beforeMiddleware: [...headers, ...redirects],
           middlewareMatchers:
             outputs.middleware?.config.matchers?.map((matcher) => ({
